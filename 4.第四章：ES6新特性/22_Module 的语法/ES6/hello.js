1eb34dc4cf21e74561a1a47112f233bd2c47ea95
“JDC2001” <2110925527@qq.com>
2023-06-10 22:19:16 +0800

    main

---
+++ 4.第四章：ES6新特性/22_Module 的语法/ES6/hello.js
@@ -0,0 +1,12 @@
+// export var Hello = "hello"
+// export var firstName = "iwen"
+
+// export function getName(){
+//     console.log(firstName);
+// }
+
+
+// 一个文件中。默认只能存在一个export default
+export default function getName(){
+    console.log("hahah");
+}
---
+++ 4.第四章：ES6新特性/22_Module 的语法/ES6/index.js
@@ -0,0 +1,15 @@
+// import { Hello,firstName,getName as getMyName } from "./hello.js"
+// import * as MyHello from "./hello"
+
+// MyHello.getName();
+// console.log(MyHello.firstName);
+
+// function getName(){
+//     console.log("哈哈哈");
+// }
+
+// getName();
+// getMyName();
+
+import CustomName from "./hello.js"
+CustomName();
